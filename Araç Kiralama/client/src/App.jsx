6e937580e1e933f8d1ce2bd19b1929c0520f5050
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-01-27 18:23:25 +0300

    .

--- Araç Kiralama/client/src/App.jsx
+++ Araç Kiralama/client/src/App.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from 'react'
 import Navbar from './components/Navbar'
-import { useLocation } from 'react-router-dom'
+import {Route,Routes ,useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import CarDetails from './pages/CarDetails'
+import Cars from './pages/Cars'
+import MyBooking from './pages/MyBooking'
+
 
 const App = () => {
 
@@ -10,6 +15,17 @@ const App = () => {
   return (
     <>
        {!isOwnerPath && <Navbar setShowLogin={setShowLogin}/>}
+
+    
+
+       <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/car-details/:id' element={<CarDetails/>}/>
+        <Route path='/cars' element={<Cars/>}/>
+        <Route path='/my-bookings' element={<MyBooking/>}/>
+       </Routes>
+
+       
     </>
   )
 }
