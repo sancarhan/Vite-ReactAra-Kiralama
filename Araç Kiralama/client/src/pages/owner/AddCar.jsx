c8a0fa0013da825e52f477297ed1fa4fc3e8563a
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-02-02 18:27:02 +0300

    .

--- Araç Kiralama/client/src/pages/owner/AddCar.jsx
+++ Araç Kiralama/client/src/pages/owner/AddCar.jsx
@@ -1,9 +1,136 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Title from '../../components/owner/Title'
+import { assets } from '../../assets/assets'
+
 
 const AddCar = () => {
+
+  const currency = import.meta.env.VITE_CURRENCY
+
+  const [image, setImage] = useState(null)
+  const [car, setCar] = useState({
+    brand: '',
+    model: '',
+    year: 0,
+    pricePerDay: 0,
+    category: '',
+    transmission: '',
+    fuel_type: '',
+    seating_capacity: 0,
+    location: '',
+    description: '',
+  })
+
+  const onSubmitHandler = async (e)=>{
+    e.preventDefault()
+  }
+
   return (
-    <div>
-      
+    <div className='px-4 py-10 md:px-10 flex-1'>
+      <Title title="Yeni Araba Ekle" subTitle='Fiyat, müsaitlik durumu ve araç özellikleri de dahil olmak üzere, rezervasyon için yeni bir araç listelemek üzere ayrıntıları doldurun.'/>
+
+      <form onSubmit={onSubmitHandler} className='flex flex-col gap-5 text-gray-500 text-sm mt-6 max-w-xl'>
+
+        {/* Araba Resmi */}
+        <div className='flex items-center gap-2 w-full'>
+          <label htmlFor="car-image">
+            <img src={image ? URL.createObjectURL(image): assets.upload_icon} alt="" className='h-14 rounded cursor-pointer'/>
+            <input type="file" id="car-image" accept='image/*' hidden onChange={e=>setImage(e.target.files[0])}/>
+          </label>
+          <p className='text-sm text-gray-500'>Aracınızın Resmini Yükleyin</p>
+        </div>
+
+        {/* Araba Markası ve Modeli */}
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+          <div className='flex flex-col w-full'>
+            <label>Marka</label>
+            <input type="text" placeholder='örn. BMW, Mercedes, Audi...' required className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none' value={car.brand} onChange={e=> setCar({...car, brand: e.target.value})}/>
+          </div>
+         
+          <div className='flex flex-col w-full'>
+            <label>Model</label>
+            <input type="text" placeholder='örn. X5, E-Class, M4...' required className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none' value={car.model} onChange={e=> setCar({...car, model: e.target.value})}/>
+          </div>
+         
+        </div>
+
+        {/* Araba Yılı, Ücret, Kategori */}
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
+           
+          <div className='flex flex-col w-full'>
+            <label>Yılı</label>
+            <input type="number" placeholder='2025' required className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none' value={car.year} onChange={e=> setCar({...car, year: e.target.value})}/>
+          </div>
+
+          <div className='flex flex-col w-full'>
+            <label>Günlük Fiyat ({currency})</label>
+            <input type="number" placeholder='2000' required className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none' value={car.pricePerDay} onChange={e=> setCar({...car, pricePerDay: e.target.value})}/>
+          </div>
+
+          <div className='flex flex-col w-full'>
+            <label>Kategori</label>
+           <select onChange={e=> setCar({...car, category: e.target.value})} value={car.category} className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none'>
+            <option value="">Bir kategori seçin</option>
+            <option value="Sedan">Sedan</option>
+            <option value="SUV">SUV</option>
+            <option value="Van">Van</option>
+           </select>
+          </div>
+
+        </div>
+
+        {/* Şanzıman Türü, Yakıt Tipi, Yolcu Kapasitesi */}
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6'>
+          
+          <div className='flex flex-col w-full'>
+            <label>Şanzıman Türü</label>
+           <select onChange={e=> setCar({...car, transmission: e.target.value})} value={car.transmission} className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none'>
+            <option value="">Şanzıman Türü Seçin</option>
+            <option value="Otomatik">Otomatik</option>
+            <option value="Manuel">Manuel</option>
+            <option value="Yarı Otomatik">Yarı Otomatik</option>
+           </select>
+          </div>
+          <div className='flex flex-col w-full'>
+            <label>Yakıt Türü</label>
+           <select onChange={e=> setCar({...car, fuel_type: e.target.value})} value={car.fuel_type} className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none'>
+            <option value="">Yakıt Türü</option>
+            <option value="Benzin">Benzin</option>
+            <option value="Dizel">Dizel</option>
+            <option value="Hybrid">Hybrid</option>
+            <option value="Elektrik">Elektrik</option>
+           </select>
+          </div>
+           <div className='flex flex-col w-full'>
+            <label>Yolcu Kapasitesi</label>
+            <input type="number" placeholder='4' required className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none' value={car.seating_capacity} onChange={e=> setCar({...car, seating_capacity: e.target.value})}/>
+          </div>
+        </div>
+
+        {/* Lokasyon */}
+        <div className='flex flex-col w-full'>
+           <label>Lokasyon</label>
+           <select onChange={e=> setCar({...car, location: e.target.value})} value={car.location} className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none'>
+            <option value="">Lokasyon</option>
+            <option value="İstanbul">İstanbul</option>
+            <option value="İzmir">İzmir</option>
+            <option value="Ankara">Ankara</option>
+            <option value="Manisa">Elektrik</option>
+           </select>
+        </div>
+        
+        {/* Araba Açıklaması */}
+         <div className='flex flex-col w-full'>
+            <label>Araç Açıklaması</label>
+            <textarea rows={5} placeholder='örn. Geniş iç mekanı ve güçlü motoruyla lüks bir SUV.' required className='px-3 py-2 mt-1 border border-borderColor rounded-md outline-none' value={car.description} onChange={e=> setCar({...car, description: e.target.value})}> 
+            </textarea>
+          </div>
+
+          <button className='flex items-center gap-2 px-4 py-2.5 mt-4 bg-primary text-white rounded-md font-medium w-max cursor-pointer'>
+            <img src={assets.tick_icon} alt="" />
+            Arabayı Listele
+          </button>
+      </form>
     </div>
   )
 }
--- Araç Kiralama/client/src/pages/owner/Dashboard.jsx
+++ Araç Kiralama/client/src/pages/owner/Dashboard.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { assets, dummyDashboardData } from '../../assets/assets'
+
 import Title from '../../components/owner/Title';
 
 const Dashboard = () => {
