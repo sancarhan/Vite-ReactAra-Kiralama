f76327d5e438680c0098efad7ac35e3951d7bf2b
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-02-02 17:21:15 +0300

    .

--- Araç Kiralama/client/src/pages/owner/Dashboard.jsx
+++ Araç Kiralama/client/src/pages/owner/Dashboard.jsx
@@ -1,9 +1,85 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { assets, dummyDashboardData } from '../../assets/assets'
+import Title from '../../components/owner/Title';
 
 const Dashboard = () => {
+
+  const currency = import.meta.env.VITE_CURRENCY
+
+  const [data, setData] = useState({
+    totalCars: 0,
+    totalBookings: 0,
+    pendingBookings: 0,
+    completedBookings: 0,
+    recentBookings: [],
+    monthlyRevenue: 0,
+  })
+
+  const dashboardCards = [
+    {title: "Araç Sayısı", value: data.totalCars, icon: assets.carIconColored},
+    {title: "Rezervasyon", value: data.totalBookings, icon: assets.listIconColored},
+    {title: "Beklemede", value: data.pendingBookings, icon: assets.cautionIconColored},
+    {title: "Onaylanmış", value: data.completedBookings, icon: assets.listIconColored},
+  ]
+
+  useEffect(()=>{
+    setData(dummyDashboardData)
+  },[])
+
   return (
-    <div>
-      
+    <div className='px-4 pt-10 md:px-10 flex-1'>
+      <Title title="Admin Paneli" subTitle="Toplam araç sayısı, rezervasyonlar, gelir ve son faaliyetler de dahil olmak üzere platformun genel performansını izleyin."/>
+
+      <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 my-8 max-w-3xl'>
+        {dashboardCards.map((card, index)=>(
+          <div key={index} className='flex gap-2 items-center justify-between p-4 rounded-md border border-borderColor'>
+            <div>
+              <h1 className='text-xs text-gray-500'>{card.title}</h1>
+              <p className='text-lg font-semibold'>{card.value}</p>
+            </div>
+            <div className='flex items-center justify-center w-10 h-10 rounded-full bg-primary/10'>
+              <img src={card.icon} alt="" className='h-4 w-4'/>
+            </div>
+          </div>
+        ))}
+      </div>
+
+      <div className='flex flex-wrap items-start gap-6 mb-8 w-full'>
+
+        {/* Son rezervasyonlar */}
+        <div className='p-4 md:p-6 border border-borderColor rounded-md max-w-lg w-full'>
+          <h1 className='text-lg font-medium'>Son Rezervasyonlar</h1>
+          <p className='text-gray-500'>En son müşteri rezervasyonları</p>
+          {data.recentBookings.map((booking, index)=>(
+            <div key={index} className='mt-4 flex items-center justify-between'>
+
+              <div className='flex items-center gap-2'>
+                <div className='hidden md:flex items-center justify-center w-12 h-12 rounded-full bg-primary/10'>
+                  <img src={assets.listIconColored} alt="" className='h-5 w-5'/>
+                </div>
+                <div>
+                  <p>{booking.car.brand} {booking.car.model}</p>
+                  <p className='text-sm text-gray-500'>{booking.createdAt.split('T')[0]}</p>
+                </div>
+              </div>
+
+              <div className='flex items-center gap-2 font-medium'>
+                <p className='text-sm text-gray-500'>{booking.price}{currency}</p>
+                <p className='px-3 py-0.5 border border-borderColor rounded-full text-sm'>{booking.status}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        
+        {/* Aylık gelir */}
+        <div className='p-4 md:p-6 mb-6 border border-borderColor rounded-md w-full md:max-w-xs'>
+          <h1 className='text-lg font-medium'>Aylık Gelir</h1>
+          <p className='text-gray-500'>Cari aya ait gelirler</p>
+          <p className='text-3xl mt-6 font-semibold text-primary'>{data.monthlyRevenue}{currency}</p>
+        </div>
+
+      </div>
+
     </div>
   )
 }
