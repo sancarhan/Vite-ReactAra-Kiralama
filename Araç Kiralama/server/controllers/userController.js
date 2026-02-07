8d5368f8663fec9df7602b639b6694010d55d514
Oğuzhan SANCAR <130090457+sancarhan@users.noreply.github.com>
2026-02-07 14:30:36 +0300

    .

---
+++ Araç Kiralama/server/controllers/userController.js
@@ -0,0 +1,56 @@
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken"
+
+
+// JWT Token
+const generateToken = (userId) =>{
+ const payload = userId;
+ return jwt.sign(payload, process.env.JWT_SECRET)
+}
+
+// Kullanıcıyı kaydet
+export const registerUser = async (req, res) => {
+  try {
+   const {name, email, password} = req.body
+
+   if(!name || !email || !password || password.length < 8){
+    return res.json({success: false, message: 'Tüm alanları doldurun'})
+   }
+
+   const userExists = await User.findOne({email})
+   if (userExists) {
+    return res.json({success: false, message: 'Kullanıcı zaten mevcut'})
+   }
+
+   const hashedPassword = await bcrypt.hash(password, 10)
+   const user = await User.create({name, email, password: hashedPassword})
+   const token = generateToken(user._id.toString())
+   res.json({success: true, token})
+
+
+  } catch (error) {
+   console.log(error.message);
+   return res.json({success: false, message: error.message})
+  }
+};
+
+// Kullanıcı giriş
+export const loginUser = async (req, res)=>{
+ try {
+   const {email, password} = req.body
+   const user =await User.findOne({email})
+   if (!user) {
+    return res.json({success: false, message: "Kullanıcı Bulunamadı"})
+   }
+   const isMatch = await bcrypt.compare(password, user.password)
+   if (!isMatch) {
+     return res.json({success: false, message: "Geçersiz kimlik bilgileri"})
+   }
+   const token = generateToken(user._id.toString())
+   res.json({success: true, token})
+ } catch (error) {
+   console.log(error.message);
+   return res.json({success: false, message: error.message})
+ }
+}
